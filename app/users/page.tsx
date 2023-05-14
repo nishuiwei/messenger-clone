7a9fa81e6335c5f8e961119f63fad330c9e04a57
卫慧杰 <w920098695@sina.cn>
2023-05-14 19:09:32 +0800

    style: emptystate component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 
-import { signOut } from 'next-auth/react'
-
+// import { signOut } from 'next-auth/react'
+import EmptyState from './../components/EmptyState'
 const Users = () => {
-	return <button onClick={() => signOut()}>Logout</button>
+	return (
+		<div className="hidden lg:block lg:pl-80 h-full">
+			<EmptyState />
+		</div>
+	)
 }
 
 export default Users
